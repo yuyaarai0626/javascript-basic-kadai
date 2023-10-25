0c0af76a48c8a2075c03e1e533c1ecf0bd361cc2
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-10-25 12:31:09 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,6 @@
-//定数の宣言・値の代入
-const height = 1.7;
-const weight = 68;
+//変数の宣言
+let height = 1.7;
+let weight = 68;
+let bmi = weight/(height * height);
 
-//出力
-console.log(weight/(height*height));
+console.log(bmi);
