d43d9a3920c282332ba614e4d2ea2b037a521181
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-10-29 13:57:21 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,16 @@
-let num = 74;
+let num = 10;
 
 //条件
-if(num % 3 === 0){
-  console.log('3の倍数です')
+if(num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
 }
-else if(num % 5 === 0){
-  console.log('5の倍数です')
+
+else if(num % 3 == 0){
+  console.log('3の倍数です');
 }
-else if(num % 3 && num % 5 ===0){
-  console.log('3と5の倍数です')
+else if(num % 5 == 0){
+  console.log('5の倍数です');
 }
 else{
-  console.log(num)
+  console.log(num);
 }
