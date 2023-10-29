cd46467cc3e3f9845ef346eef5764cc140820829
yuyaarai0626 <148346894+yuyaarai0626@users.noreply.github.com>
2023-10-29 20:58:21 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,11 @@
+let holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+
+for(let index = 0; index < 16; index += 1){
+  console.log(holidays[index]);
+}
+
+let i =0;
+while(i<15){
+  i += 1;
+  console.log(holidays[i]);
+}
